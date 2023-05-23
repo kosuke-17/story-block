0d439a5622a2af616a40c5ac2f65248d65e6667a
kosuke-17 <tamutamu17@icloud.com>
2023-05-24 08:07:54 +0900

    add sorrcer field

---
+++ src/components/SoccerField/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SoccerField = () => {
+  return <div>SoccerField</div>
+}
+
+export default SoccerField
---
+++ src/stories/SoccerField.stories.tsx
@@ -0,0 +1,6 @@
+import SoccerField from '@/components/SoccerField'
+import { ComponentStory } from '@storybook/react'
+
+const Template: ComponentStory<typeof SoccerField> = (args) => <SoccerField />
+
+export const Primary = Template.bind({})
