7750533def8ae41a9a65022df92dd740bf95ef16
kosuke-17 <tamutamu17@icloud.com>
2023-05-28 11:18:31 +0900

    styledを使ってコンポーネント分離

--- src/components/SoccerField/index.tsx
+++ src/components/SoccerField/index.tsx
@@ -2,77 +2,72 @@ import React from 'react'
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+
+const centerCSS = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const SoccerFieldLayout = styled(Box)(() => ({
+  ...centerCSS,
+  width: '100vw',
+  height: '100vh',
+}))
+
+const StyledPaper = styled(Paper)(() => ({
+  width: '900px',
+  height: '90%',
+}))
+
+const CornerLeftTopArc = styled(Box)(() => ({
+  width: '20px',
+  height: '20px',
+  top: -3,
+  left: -3,
+  border: '3px solid #FFFFFF',
+  borderBottomRightRadius: '60%',
+  position: 'absolute',
+}))
+
+const GreenField = styled(Box)(({ theme }) => ({
+  ...centerCSS,
+  width: '840px',
+  height: '630px',
+  backgroundColor: theme.palette.primary.main,
+}))
+
+const SoccerFieldOutLine = styled(Box)(() => ({
+  ...centerCSS,
+  border: '3px solid #FFFFFF',
+  width: '720px',
+  height: '540px',
+  position: 'relative',
+}))
+
+const SoccerFieldCenterLine = styled(Box)(() => ({
+  ...centerCSS,
+  backgroundColor: '#FFFFFF',
+  width: '3px',
+  height: '100%',
+}))
 
 const SoccerField = () => {
-  const centerCSS = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
   return (
-    <Box
-      sx={{
-        width: '100vw',
-        height: '100vh',
-        ...centerCSS,
-      }}
-    >
-      <Paper
-        sx={{
-          width: '900px',
-          height: '700px',
-          px: 3,
-          pb: 3,
-        }}
-        elevation={3}
-      >
+    <SoccerFieldLayout>
+      <StyledPaper elevation={3}>
         <Typography variant='h2'>サッカー場</Typography>
         <Box sx={centerCSS}>
-          {/* GreenField */}
-          <Box
-            sx={{
-              ...centerCSS,
-              width: '840px',
-              height: '630px',
-              bgcolor: 'primary.main',
-            }}
-          >
-            {/* SoccerFieldOutLine */}
-            <Box
-              sx={{
-                ...centerCSS,
-                border: '3px solid #FFFFFF',
-                width: '720px',
-                height: '540px',
-                position: 'relative',
-              }}
-            >
-              {/* Corner Arc */}
-              <Box
-                sx={{
-                  width: '20px',
-                  height: '20px',
-                  top: -3,
-                  left: -3,
-                  border: '3px solid #FFFFFF',
-                  borderBottomRightRadius: '60%',
-                  position: 'absolute',
-                }}
-              />
-              {/* SoccerFieldCenterLine */}
-              <Box
-                sx={{
-                  ...centerCSS,
-                  bgcolor: '#FFFFFF',
-                  width: '3px',
-                  height: '100%',
-                }}
-              />
-            </Box>
-          </Box>
+          <GreenField>
+            <SoccerFieldOutLine>
+              <CornerLeftTopArc />
+              <SoccerFieldCenterLine />
+            </SoccerFieldOutLine>
+          </GreenField>
         </Box>
-      </Paper>
-    </Box>
+      </StyledPaper>
+    </SoccerFieldLayout>
   )
 }
 
