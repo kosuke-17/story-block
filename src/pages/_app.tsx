6b042f44b6ff712e3c97cd5047c1216f6e1e6db9
kosuke-17 <tamutamu17@icloud.com>
2023-05-28 09:52:36 +0900

    muiのthemeをrootに設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import type { AppProps } from 'next/app'
 
+import theme from '@/styles/theme'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
--- src/styles/theme/palette.ts
+++ src/styles/theme/palette.ts
@@ -1,15 +1,5 @@
-import { cyan, grey, orange, common } from '@mui/material/colors'
-
 export const palette = {
   primary: {
-    main: orange[500],
-    dark: orange[400],
-    light: grey[50],
+    main: '#228B22',
   },
-  secondary: {
-    main: cyan[300],
-    dark: cyan[700],
-    light: cyan[200],
-  },
-  grey,
 }
